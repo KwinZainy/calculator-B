a2f8208aba5cd2676d62868fddca873076312827
KwinZainy <gbadamosiblessing651@gmail.com>
2025-06-02 10:32:16 +0100

    the multiplication btn

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let btn = document.querySelectorAll("btn")
+let btn = document.querySelectorAll(".btn")
 let input = document.getElementById("input")
+let mult = document.getElementById("multiplication")
+btn.forEach(element => {
+    element.addEventListener('click', () => {
 
-// function inputText(val){
-//     return input.value = input.value + val
-// }
+        let text = element.textContent
+        if(text == 'x'){
+        text = '*'  
+    }      
+    input.textContent += text
+        console.log(element); 
+    })
+    
+})
+
